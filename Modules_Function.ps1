b29248bf5c4fba75aede6a2688489be88dd106bb
Sabri CHARCHOUF <barioucha@gmail.com>
2024-03-18 16:05:03 +0100

    First commit

---
+++ Modules_Function.ps1
@@ -0,0 +1,37 @@
+function Import-ModulesIfNotExists {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string[]]$ModuleNames
+    )
+    # Check for missing modules
+    $missingModules = $ModuleNames | Where-Object { -not (Get-Module -Name $_ -ListAvailable) }
+    if ($missingModules) {
+        Write-Host "Critical modules missing: $($missingModules -join ', ')" -ForegroundColor Red
+        Write-Host "The script cannot continue without these modules. Please ensure they are installed locally and try again." -ForegroundColor Red
+        Exit 1  # Exit with error code 1
+    }
+    # Import remaining modules
+    $totalModules = $ModuleNames.Count
+    $currentModuleNumber = 0
+    foreach ($ModuleName in $ModuleNames) {
+        $currentModuleNumber++
+        if (Get-Module -Name $ModuleName -ListAvailable) {
+            Write-Host "$ModuleName is already imported" -ForegroundColor Yellow
+        } else {
+            $progress = ($currentModuleNumber / $totalModules) * 100
+            Write-Progress -Activity "Importing $ModuleName" -Status "Please wait..." -PercentComplete $progress
+            Import-Module -Name $ModuleName -ErrorAction SilentlyContinue -ErrorVariable importError
+            if ($importError) {
+                Write-Host "Failed to import $ModuleName $($importError.Exception.Message)" -ForegroundColor Red
+            } else {
+                Write-Host "Successfully imported $ModuleName" -ForegroundColor Green
+            }
+        }
+    }
+    # Clear progress bar (optional)
+    Write-Progress -Activity "Importing modules" -Completed
+    Write-Host "`nDone checking and importing modules."
+}
+# Import the required modules
+$modules = @('HPEOneView.850', 'Microsoft.PowerShell.Security', 'Microsoft.PowerShell.Utility')
+Import-ModulesIfNotExists -ModuleNames $modules
