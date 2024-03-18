5dafda894cbd3747772b7eec5510d7aea0d67809
Sabri CHARCHOUF <barioucha@gmail.com>
2024-03-18 17:20:13 +0100

    Add description and change script

--- Modules_Function.ps1
+++ Modules_Function.ps1
@@ -1,13 +1,39 @@
+<#
+.SYNOPSIS
+This script contains a function to import required modules if they are not already imported.
+
+.DESCRIPTION
+The Import-ModulesIfNotExists function checks if the required modules are already imported. 
+If any of the required modules are missing, it displays an error message and exits the script. Otherwise, it imports the remaining modules.
+
+.PARAMETER ModuleNames
+Specifies an array of module names that need to be imported.
+
+.EXAMPLE
+$RequiredModules = @('HPEOneView.850', 'Microsoft.PowerShell.Security', 'Microsoft.PowerShell.Utility')
+Import-ModulesIfNotExists -ModuleNames $RequiredModules
+
+This example imports the required modules specified in the $RequiredModules array using the Import-ModulesIfNotExists function.
+
+.NOTES
+Author: CHARCHOUF SABRI
+Date:   14/03/2024
+Version : 1.0
+#>
 function Import-ModulesIfNotExists {
     param (
         [Parameter(Mandatory=$true)]
         [string[]]$ModuleNames
     )
     # Check for missing modules
+    Write-Host "`tChecking for missing modules..." -ForegroundColor Cyan
+    # Check if the required modules are already imported
     $missingModules = $ModuleNames | Where-Object { -not (Get-Module -Name $_ -ListAvailable) }
+    # If there are missing modules, display a message and exit the script
     if ($missingModules) {
-        Write-Host "Critical modules missing: $($missingModules -join ', ')" -ForegroundColor Red
-        Write-Host "The script cannot continue without these modules. Please ensure they are installed locally and try again." -ForegroundColor Red
+        Write-Host "`tCritical modules missing: " -NoNewline -ForegroundColor Red
+        Write-Host "$($missingModules -join ', ')" -ForegroundColor Yellow
+        Write-Host "`tThe script cannot continue without these missing module(s). Please ensure they are installed locally and try again." -ForegroundColor Red
         Exit 1  # Exit with error code 1
     }
     # Import remaining modules
