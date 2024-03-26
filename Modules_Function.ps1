0413919354f2f9e3d5e57e3cba3d945a0dfff516
Sabri Charchouf <barioucha@gmail.com>
2024-03-26 09:36:46 +0100

    Update Modules_Function.ps1
    Update the function

--- Modules_Function.ps1
+++ Modules_Function.ps1
@@ -19,21 +19,28 @@ This example imports the required modules specified in the $RequiredModules arra
 Author: CHARCHOUF SABRI
 Date:   14/03/2024
 Version : 1.0
-#>
+#># Assuming the Logging folder is at the same level as the Scripts folder
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$loggingFunctionsPath = Join-Path -Path $scriptPath -ChildPath "..\Logging\Logging_Functions.ps1"
+
+# Source the logging functions
+. $loggingFunctionsPath
+
+# Define the function to import modules if they are not already present
 function Import-ModulesIfNotExists {
     param (
         [Parameter(Mandatory=$true)]
         [string[]]$ModuleNames
     )
     # Check for missing modules
-    Write-Host "`tChecking for missing modules..." -ForegroundColor Cyan
+    Write-Log "`tChecking for missing modules..." -ForegroundColor Cyan
     # Check if the required modules are already imported
     $missingModules = $ModuleNames | Where-Object { -not (Get-Module -Name $_ -ListAvailable) }
     # If there are missing modules, display a message and exit the script
     if ($missingModules) {
-        Write-Host "`tCritical modules missing: " -NoNewline -ForegroundColor Red
-        Write-Host "$($missingModules -join ', ')" -ForegroundColor Yellow
-        Write-Host "`tThe script cannot continue without these missing module(s). Please ensure they are installed locally and try again." -ForegroundColor Red
+        Write-Log "`tCritical modules missing: " -NoNewline -ForegroundColor Red
+        Write-Log "$($missingModules -join ', ')" -ForegroundColor Yellow
+        Write-Log "`tThe script cannot continue without these missing module(s). Please ensure they are installed locally and try again." -ForegroundColor Red
         Exit 1  # Exit with error code 1
     }
     # Import remaining modules
@@ -42,26 +49,30 @@ function Import-ModulesIfNotExists {
     foreach ($ModuleName in $ModuleNames) {
         $currentModuleNumber++
         if (Get-Module -Name $ModuleName -ListAvailable) {
-            Write-Host "`tThe module " -NoNewline -foregroundColor Yellow
-            Write-Host "[$ModuleName] " -NoNewline -foregroundColor Cyan
-            Write-Host "is already imported." -ForegroundColor Yellow
+            Write-Log "`tThe module " -NoNewline -foregroundColor Yellow
+            Write-Log "[$ModuleName] " -NoNewline -foregroundColor Cyan
+            Write-Log "is already imported." -ForegroundColor Yellow
         } else {
             $progress = ($currentModuleNumber / $totalModules) * 100
             Write-Progress -Activity "Importing $ModuleName" -Status "Please wait..." -PercentComplete $progress
             Import-Module -Name $ModuleName -ErrorAction SilentlyContinue -ErrorVariable importError
             if ($importError) {
-                Write-Host "`tFailed to import The module " -NoNewline -ForegroundColor Red
-                Write-Host "$ModuleName $($importError.Exception.Message)" -ForegroundColor Red
+                Write-Log "`tFailed to import The module " -NoNewline -ForegroundColor Red
+                Write-Log "$ModuleName $($importError.Exception.Message)" -ForegroundColor Red
             } else {
-                Write-Host "`tSuccessfully imported The module" -NoNewline -ForegroundColor Green
-                Write-Host "[$ModuleName]" -ForegroundColor Green
+                Write-Log "`tSuccessfully imported The module" -NoNewline -ForegroundColor Green
+                Write-Log "[$ModuleName]" -ForegroundColor Green
             }
         }
     }
     # Clear progress bar (optional)
     Write-Progress -Activity "Importing modules" -Completed
-    Write-Host "`n`tDone checking and importing modules." -ForegroundColor Green
+    Write-Log "`n`tDone checking and importing modules." -ForegroundColor Green
 }
+
 # Import the required modules
 $RequiredModules = @('HPEOneView.850', 'Microsoft.PowerShell.Security', 'Microsoft.PowerShell.Utility')
 Import-ModulesIfNotExists -ModuleNames $RequiredModules
+
+# Now you can use logging functions like Write-Log and the imported modules
+
